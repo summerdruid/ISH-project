373faf247e4bc90c678b852fcb17ce34ade8d323
Michelle <michelle@Computer.local>
2017-11-16 20:42:50 +0000

    added basic gpx parser to views.py

--- LuckyGPX/viewer/views.py
+++ LuckyGPX/viewer/views.py
@@ -4,6 +4,8 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from viewer.forms import UserForm
+import xml.etree.ElementTree as ET
+
 
 def index(request):
     context_dict = {}
@@ -56,3 +58,24 @@ def test(request):
     context_dict = {'boldmessage':  "Bolf font message!"}
 
     return render(request, 'viewer/test.html', context_dict)
+
+
+# takes gpx data as a string, returns list of dictionaries with
+# lat and long of each point on the first track of the gpx data.
+def parseGPX(data):
+    gpxstring = data
+
+    namespace = {'gpx': 'http://www.topografix.com/GPX/1/1'}
+    root = ET.fromstring(gpxstring)
+    track = root.findall(".//gpx:trk", namespace)
+    trkseg  = root.find(".//gpx:trkseg", namespace)
+    points = trkseg.findall(".gpx:trkpt", namespace)
+    parsed_data = []
+    for point in points:
+        lat = float(point.attrib['lat'])
+        lon = float(point.attrib['lon'])
+        parsed_data.append({'lat': lat, 'lon': lon})
+
+    return parsed_data
+        
+
