e363a483d7bac5338c4c94797f3db29beb8c29a0
Michelle <michelle@Computer.local>
2017-11-16 15:22:16 +0000

    Added user profile model, getting towards having login capability

--- LuckyGPX/viewer/models.py
+++ LuckyGPX/viewer/models.py
@@ -2,12 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    def __unicode__(self):
+        return self.user.username
 
 class Route(models.Model):
     name = models.CharField(max_length=128, unique=True)
-
+    owner = models.ForeignKey(UserProfile, null=True)
     def __unicode__(self):
         return self.name
 
