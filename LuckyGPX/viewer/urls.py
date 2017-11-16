730ea3df3eec6cdab08aefc74e62277b37edd6e4
2179147m <2179147m@student.gla.ac.uk>
2017-11-16 17:29:30 +0000

    Added login to urls.py

--- LuckyGPX/viewer/urls.py
+++ LuckyGPX/viewer/urls.py
@@ -3,4 +3,5 @@ from viewer import views
 
 urlpatterns = [url(r'^$', views.index, name='index'),
               url(r'^register/$',views.register, name='register'),
-              url(r'^newindex/$', views.newindex, name='newindex'),]
+              url(r'^newindex/$', views.newindex, name='newindex'),
+			  url(r'^login/$', views.login, name='login')]
