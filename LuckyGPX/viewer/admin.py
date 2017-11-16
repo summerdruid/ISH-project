3f378bddc9a25f7e2f7c66dc9e8224c17a66ae9a
Michelle <michelle@Computer.local>
2017-11-16 13:27:32 +0000

    Added first attempt at db models. Set up admin interface, can access via /admin need to create super user using python manage.py createsuperuser

--- LuckyGPX/viewer/admin.py
+++ LuckyGPX/viewer/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from viewer.models import Route, Run, Point
 
 # Register your models here.
+admin.site.register(Route)
+admin.site.register(Run)
+admin.site.register(Point)
--- LuckyGPX/viewer/models.py
+++ LuckyGPX/viewer/models.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Route(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __unicode__(self):
+        return self.name
+
+class Point(models.Model):
+    longitude = models.IntegerField
+    latitude = models.IntegerField
+
+class Run(models.Model):
+    route = models.ForeignKey(Route)
+    points = models.ManyToManyField(Point)
+    startTime = models.DateField()
